763440ba524ec53382b1df3c72ffe51966f8e8ee
May Ng <mngmay@gmail.com>
2019-06-14 08:45:02 -0700

    Added closure explanation.

--- challenges/functions.js
+++ challenges/functions.js
@@ -8,7 +8,7 @@
 */
 
 function consume(param1, param2, cb){
-  return cb(param1, param2);
+  console.log(cb(param1, param2));
 }
 
 /* Step 2: Create several functions to callback with consume();
@@ -17,11 +17,22 @@ function consume(param1, param2, cb){
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -29,7 +40,8 @@ function consume(param1, param2, cb){
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+/*Because of closure, the nested function is able to access variables declared within its direct function scope, AS WELL AS the variables
+in outer scopes such as internal in myFunction, and global scope variables.  Think of it as being able to access from your level outward, but you can't access further in to the VIP area.*/
 
 const external = "I'm outside the function";
 
